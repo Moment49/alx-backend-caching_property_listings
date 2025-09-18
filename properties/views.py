781d5d67077d5d3bbafd57a5ca56b88188f404b9
moment49 <ibenachoelvis49@gmail.com>
2025-09-18 16:58:33 +0100

    updated

--- properties/views.py
+++ properties/views.py
@@ -18,9 +18,9 @@ from django.http import JsonResponse
 @cache_page(60 * 15)  # cache for 15 minutes in Redis
 def property_list(request):
     """Return all properties and cache the response for 15 minutes."""
-    queryset = Property.objects.all()
+    queryset = get_all_properties()
     serializer = PropertySerializer(queryset, many=True)
-    return JsonResponse({"properties": serializer.data}, safe=False)
+    return JsonResponse(serializer.data, safe=False)
 
 
 
