20033579d2cd104e887a08bc81efc4c4455d3bf9
moment49 <ibenachoelvis49@gmail.com>
2025-09-18 16:46:34 +0100

    updated

--- properties/views.py
+++ properties/views.py
@@ -13,11 +13,11 @@ from rest_framework.response import Response
 from .serializers import PropertySerializer
 from .utils import get_all_properties
 
-
 @api_view(['GET'])
+@cache_page(60 * 15)  # cache for 15 minutes in Redis
 def property_list(request):
-    """Return all properties, cached in Redis for 1 hour."""
-    queryset = get_all_properties()
+    """Return all properties and cache the response for 15 minutes."""
+    queryset = Property.objects.all()
     serializer = PropertySerializer(queryset, many=True)
     return Response(serializer.data)
 
